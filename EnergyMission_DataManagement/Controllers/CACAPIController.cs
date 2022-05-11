e7b35795d18acf9ecf6b574502524fa957899e17
Anup Damodaran <anup.damoda@gmail.com>
2022-05-11 23:09:55 +1000

    Adding Post call for CAC (Insert data via Post call)

--- EnergyMission_DataManagement/Controllers/CACAPIController.cs
+++ EnergyMission_DataManagement/Controllers/CACAPIController.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
+
 namespace AceSchoolPortal.Controllers
 {
     [Route("api/[controller]")]
@@ -27,7 +28,7 @@ namespace AceSchoolPortal.Controllers
             this.db = db;
         }
         
-        [HttpGet]
+        [System.Web.Mvc.HttpGet]
         [ProducesResponseType(200)]
         [ProducesResponseType(400)]
         public ActionResult<IEnumerable<CustAccntCode>> CACAPI()
@@ -38,10 +39,38 @@ namespace AceSchoolPortal.Controllers
             }
             catch (Exception ex)
             {
-                
                 return BadRequest("Failed to get cacs");
             }
         }
 
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult CACAPI(CACViewModel model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data.");
+
+                var newCAC = new CustAccntCode()
+                {
+                    CAC_number = model.CAC_number,
+                    nmi_number = model.NMI_number,
+                    UsedForContract = model.UsedForContract,
+                    created_at = DateTime.Now,
+                    updated_at = DateTime.Now
+                };
+
+                _repository.AddEntity(newCAC);
+                _repository.SaveAll();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to get cacs");
+            }
+        }
     }
 }
